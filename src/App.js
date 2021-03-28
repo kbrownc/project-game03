eb941fe27129f974f0b1604d4b75c4e4556fac16
Kim Brown <kbrownc341@gmail.com>
2021-03-28 13:10:16 -0400

    Lukes suggestions added in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
+//import React, { Component } from 'react';
 import './App.css';
 import {newBoard} from './newboard.js';
-
-let message = "Roll again";
-let optMessage = "Kim";
-let score = 0;
 
 class App extends React.Component {
   state = {
     roll: 0,
+    message: "Roll again",
+    optMessage: "Kim",
     score: 0,
     position: -1,
     board: newBoard.slice()
   };
 // reset game (Play button)
   onReset = () => {
-   message = "Roll again";
-   optMessage = "Kim";
-   score = 0;
     this.setState(({
       roll: null,
+      message: "Roll again",
+      optMessage: "Kim",
       score: 0,
       position: -1,
       board: newBoard.slice()
@@ -28,12 +26,15 @@ class App extends React.Component {
 // roll dice (roll button)
   onRoll = () => {
     const randomNumber = Math.floor(Math.random() * 6) + 1;
+    let workBoard = this.state.board.slice();
     let scoreAdj = 1;
-    optMessage = "Kim";
+    let newPosition = Math.min(this.state.position + randomNumber, workBoard.length - 1)
+    let message = "Roll again";
+    let optMessage = "Kim";
     // Need to adjust score if 'miss a turn' or 'gain a turn' was landed on
-    if (this.state.board[Math.min(this.state.position + randomNumber, this.state.board.length - 1)].extraScore != undefined){
-        scoreAdj = scoreAdj + this.state.board[this.state.position + randomNumber].extraScore;
-        if (this.state.board[this.state.position + randomNumber].extraScore < 0){
+    if (workBoard[newPosition].extraScore !== undefined){
+        scoreAdj = scoreAdj + workBoard[newPosition].extraScore;
+        if (workBoard[newPosition].extraScore < 0){
             optMessage = "Wonderful....you get an extra roll";
           }
           else {
@@ -41,32 +42,28 @@ class App extends React.Component {
           }
       };
     // Add the detour squares and remove a single square after detour
-    if (this.state.board[Math.min
-    (this.state.position + randomNumber, this.state.board.length - 1)  ].itemsToAdd!= undefined){  
-        this.state.board.splice(this.state.position + randomNumber + 1, 1 ,  
-            this.state.board[this.state.position + randomNumber].itemsToAdd[0],
-            this.state.board[this.state.position + randomNumber].itemsToAdd[1],
-            this.state.board[this.state.position + randomNumber].itemsToAdd[2],
-            this.state.board[this.state.position + randomNumber].itemsToAdd[3],
-            this.state.board[this.state.position + randomNumber].itemsToAdd[4],
-            this.state.board[this.state.position + randomNumber].itemsToAdd[5],
-            this.state.board[this.state.position + randomNumber].itemsToAdd[6]);
-        optMessage = "You have a longer journey";
-    }
+     if (workBoard[newPosition].itemsToAdd !== undefined){ 
+        let workBoard2 = workBoard.slice(0, newPosition + 1).
+                    concat(workBoard[newPosition].itemsToAdd.slice(0), 
+                     workBoard.slice(newPosition + 1 + workBoard[newPosition].itemsToDelete));
+        workBoard = workBoard2.slice();
+         optMessage = "You have a longer journey";
+     }
     // Check for end of Game
-    if ((this.state.position + 1) >= (this.state.board.length - 1)) {
+    if ((newPosition) >= (workBoard.length - 1)) {
       message = "Game Complete";
       optMessage = "Kim";
     }
     else {
         message = "Role Again";
     }
-    score = score + scoreAdj;
     this.setState((oldState) => ({
       roll: randomNumber,
-      score: score,
-      position: Math.min(oldState.position + randomNumber, this.state.board.length - 1),
-      board: this.state.board.slice()
+      message: message,
+      optMessage: optMessage,
+      score: oldState.score + scoreAdj,
+      position: newPosition,
+      board: workBoard
     }));
   }
 // render
@@ -74,22 +71,23 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="Nav">      
-          <div className="Box Butt" style={{gridColumn: 1, gridRow: 1}} onClick={this.onReset}>Play</div>
+          <div className="Box Button" style={{gridColumn: 1, gridRow: 1}} onClick={this.onReset}>Play</div>
           <div className="Box" style={{gridColumn: 6, gridRow: 1}}>Score</div>
           <div className="Box" style={{gridColumn: 7, gridRow: 1}}>{this.state.score}</div>
         </div>
         <div className="Messages">       
-              <h4>{message}</h4>
-              <h5>{optMessage}</h5>
+              <h4>{this.state.message}</h4>
+              <h5>{this.state.optMessage}</h5>
         </div>
         <div className="Board">
-            <div 
-              className={`Butt Box${this.state.position <= this.state.board.length - 2 ? "" : 'Gone'}`}
-              style={{gridColumn: 4, gridRow: 1}} 
-              onClick={this.onRoll}>Roll</div>
-            <div 
-              className={`Box${this.state.position <= this.state.board.length - 2 ? "" : 'Gone'}`}
-              style={{gridColumn: 4, gridRow: 2}}>{this.state.roll}</div>
+              {this.state.position <= this.state.board.length - 2 ? (
+                <div className={'Button Box'} style={{gridColumn: 4, gridRow: 1}} 
+                onClick={this.onRoll}>Roll</div>
+                ) : null}
+
+              {this.state.position <= this.state.board.length - 2 ? (
+                <div className={'Button Box'} style={{gridColumn: 4, gridRow: 2}}>
+                {this.state.roll}</div>) : null}
           {
               this.state.board.map((item, index) => (
                 <div key={index}
