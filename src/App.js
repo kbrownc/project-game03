46543fabd493c6209c82319aee0abd9416d17155
Kim Brown <kbrownc341@gmail.com>
2021-04-11 12:52:01 -0400

    Initial version - functional component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'; 
 import { newBoard } from './newboard.js';
 
-class App extends React.Component {
-  state = {
-    roll: 0,
-    message: 'Roll again',
-    optMessage: 'Kim',
-    score: 0,
-    position: -1,
-    board: newBoard.slice(),
-  };
+function App() {
+  const [roll, setRoll] = useState(0);
+  const [message, setMessage] = useState("Roll again");
+  const [optMessage, setOptMessage] = useState("Kim");
+  const [score, setScore] = useState(0);
+  const [position, setPosition] = useState(-1);
+  const [board, setBoard] = useState(newBoard.slice());
+  
   // reset game (Play button)
   onReset = () => {
-    this.setState({
-      roll: null,
-      message: 'Roll again',
-      optMessage: 'Kim',
-      score: 0,
-      position: -1,
-      board: newBoard.slice(),
-    });
+    setRoll(null);
+    setMessage("Roll again");
+    setOptMessage("Kim");
+    setScore(0);
+    setPosition(-1);
+    setBoard(newBoard.slice());
   };
   // roll dice (roll button)
   onRoll = () => {
-    this.setState(oldState => {
-      let workBoard = oldState.board.slice();
+      let workBoard = board.slice();
       const randomNumber = Math.floor(Math.random() * 6) + 1;
       let scoreAdj = 1;
-      let newPosition = Math.min(oldState.position + randomNumber, workBoard.length - 1);
+      let newPosition = Math.min(position + randomNumber, workBoard.length - 1);
       let message = 'Roll again';
       let optMessage = 'Kim';
       // Need to adjust score if 'miss a turn' or 'gain a turn' was landed on
@@ -58,19 +54,17 @@ class App extends React.Component {
       } else {
         message = 'Role Again';
       }
-      return {
-        board: workBoard,
-        roll: randomNumber,
-        message: message,
-        optMessage: optMessage,
-        score: oldState.score + scoreAdj,
-        position: newPosition,
-      };
-    });
+ //     return {
+        setRoll(randomNumber)
+        setMessage(message)                        // need new name
+        setOptMessage(optMessage)                  // need new name
+        setScore(prevScore => prevScore + scoreAdj)
+        setPosition(newPosition)
+        setBoard(workBoard)
+ //     };
   };
-  // render
-  render() {
-    let isGameComplete = this.state.position <= this.state.board.length - 2;
+  // return
+    let isGameComplete = position <= board.length - 2;
     return (
       <div className="App">
         <div className="Nav">
@@ -81,12 +75,12 @@ class App extends React.Component {
             Score
           </div>
           <div className="Box" style={{ gridColumn: 7, gridRow: 1 }}>
-            {this.state.score}
+            {score}
           </div>
         </div>
         <div className="Messages">
-          <h4>{this.state.message}</h4>
-          <h5>{this.state.optMessage}</h5>
+          <h4>{message}</h4>
+          <h5>{optMessage}</h5>
         </div>
         <div className="Board">
           {isGameComplete ? (
@@ -97,13 +91,13 @@ class App extends React.Component {
 
           {isGameComplete ? (
             <div className={'Button Box'} style={{ gridColumn: 4, gridRow: 2 }}>
-              {this.state.roll}
+              {roll}
             </div>
           ) : null}
-          {this.state.board.map((item, index) => (
+          {board.map((item, index) => (
             <div
               key={index}
-              className={`Box${this.state.position === index ? ' markSpot' : ''}`}
+              className={`Box${position === index ? ' markSpot' : ''}`}
               style={{ gridColumn: `${item.boxCol}`, gridRow: `${item.boxRow}` }}
             >
               {item.boxContent}
@@ -113,6 +107,5 @@ class App extends React.Component {
       </div>
     );
   }
-}
 
 export default App;
